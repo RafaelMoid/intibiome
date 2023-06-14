1bf9f5e106f96db8de11eec051983ce42719e506
RafaelMoid <rafaelvrcr@gmail.com>
2023-06-14 15:55:11 -0300

    Footer done

--- footer.php
+++ footer.php
@@ -1,7 +1,23 @@
-<footer id="colophon" class="site-footer">
-		
+<footer id="footer" class="site-footer">
+    <ul class="navi-footer">
+        <li><a href="">contact us</a></li>
+        <li><a href="">faq</a></li>
+        <li><a href="">site map</a></li>
+        <li><a href="">privacy policy</a></li>
+        <li><a href="">cookies policy</a></li>
+        <li><a href="">legal notice</a></li>
+    </ul>
+    <div class="logos-wrapper">
+        <a href="">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Image 1.png" class="icon-ulabs">
+        </a>
+        <a href="">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Layer 2.png" class="icon-insta">
+        </a>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
